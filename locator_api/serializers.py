13e57c359be20cfb70e3743c18ec99c25650dcb0
Joseph Kariuki <joehene@gmail.com>
2023-08-09 21:09:40 +0300

    Created MPESA locations serializer.

---
+++ locator_api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from rest_framework_gis.serializers import GeoFeatureModelSerializer, GeometryField
+
+from .models import MpesaLocations
+
+
+class MpesaLocationsSerializer(GeoFeatureModelSerializer):
+    geom = GeometryField()
+    class Meta:
+        model = MpesaLocations
+        geo_field = "geom"
+        fields = [
+            "formatted_field", "input_stri", "county"
+        ]
+        read_only_fields = ["id", "latitude", "longitude"]
