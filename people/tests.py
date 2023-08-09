2003c8c04f008eb65504a66ebe8f18e34944bc7b
Joseph Kariuki <joehene@gmail.com>
2023-08-09 15:39:57 +0300

    created uni tests for people app.

--- people/tests.py
+++ people/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="jkariuki",
+            email="jkariuki@email.com",
+            password="somepassword",
+        )
+        self.assertEqual(user.username, "jkariuki")
+        self.assertEqual(user.email, "jkariuki@email.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+        try:
+            self.assertIsNotNone(user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+        with self.assertRaises(TypeError):
+            User.objects.create_user(email="")
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username="mzito", email="mzito@email.com", password="superpassword"
+        )
+        self.assertEqual(admin_user.username, "mzito")
+        self.assertEqual(admin_user.email, "mzito@email.com")
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+        try:
+            self.assertIsNotNone(admin_user.username)
+        except AttributeError:
+            pass
