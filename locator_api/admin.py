0425d8c2a693df630e54941e5e45d3d58d542482
Joseph Kariuki <joehene@gmail.com>
2023-08-09 21:08:56 +0300

    Registered MPESA model to admin site.

--- locator_api/admin.py
+++ locator_api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from leaflet.admin import LeafletGeoAdmin
+from .models import MpesaLocations
 
-# Register your models here.
+
+class MpesaLocationsAdmin(LeafletGeoAdmin):
+    list_display = ("formatted_field", "input_stri", "county")
+
+
+admin.site.register(MpesaLocations, MpesaLocationsAdmin)
