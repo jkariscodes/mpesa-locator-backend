2ab353cb124c787aed0d2c452ebfde894e644c77
Joseph Kariuki <joehene@gmail.com>
2023-08-09 21:08:25 +0300

    Added MPESA locations model.

--- locator_api/models.py
+++ locator_api/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.contrib.gis.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class MpesaLocations(models.Model):
+    formatted_field = models.CharField(_("Name"), max_length=254)
+    latitude = models.FloatField(_("Latitude"))
+    longitude = models.FloatField(_("Longitude"))
+    input_stri = models.CharField(_("Detailed Name"), max_length=254)
+    county = models.CharField(_("County"), max_length=20)
+    geom = models.MultiPointField(_("Geometry"))
+
+    def __str__(self):
+        return self.formatted_field
