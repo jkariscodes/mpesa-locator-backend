4cbece754c92db26948cc98c5a1c80aca09a908e
Joseph Kariuki <joehene@gmail.com>
2023-08-12 08:16:01 +0300

    Added django toolbar for testing in development.

--- locator_backend/urls.py
+++ locator_backend/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import (
@@ -34,3 +36,9 @@ urlpatterns = [
         name="swagger-ui",
     ),
 ]
+if settings.ENV == "development":
+    import debug_toolbar
+
+    urlpatterns = [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ] + urlpatterns
